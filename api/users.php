636d939d3bbc2badd4182f1b38886be38e7c1269
Harris Home <sensorsoffline@gmail.com>
2026-04-12 23:59:09 +0500

    Added Filter & User Search

--- api/users.php
+++ api/users.php
@@ -6,6 +6,51 @@ switch($_SERVER['REQUEST_METHOD']) {
 
 CASE 'GET':
 
+
+// Filter Record by Status
+
+if(isset($_GET['filter_status'])) {
+    if(is_authorized(2) || is_authorized(3)) {
+        $term = $_GET['filter_status'];
+
+        $sql = mysqli_prepare(con, "SELECT * FROM `users` WHERE status = ?");
+        mysqli_stmt_bind_param($sql, "s", $term);
+        mysqli_stmt_execute($sql);
+        $res = mysqli_stmt_get_result($sql);
+        while($row = mysqli_fetch_assoc($res)) {
+            // Enter HTML return for Search
+        }
+
+    }
+}
+
+
+
+
+
+
+
+// Returns search result for a User
+
+if(isset($_GET['search'])) {
+    if(is_authorized(2) || is_authorized(3)) {
+        $term = $_GET['search'];
+
+        $sql = mysqli_prepare(con, "SELECT * FROM `users` WHERE (name LIKE ? OR email LIKE ? OR phone LIKE ?)");
+        mysqli_stmt_bind_param($sql, "s", $term);
+        mysqli_stmt_execute($sql);
+        $res = mysqli_stmt_get_result($sql);
+        while($row = mysqli_fetch_assoc($res)) {
+            // Enter HTML return for Search
+        }
+
+    }
+}
+
+
+
+
+
 // Returns single User from ID
 
 if(isset($_GET['id'])) {
@@ -93,20 +138,31 @@ else if (is_authorized(2) || is_authorized(3)) {$usr = $_POST['email'];}
 
 
 $table_posted = [];
+$valueName = [];
+$valueData = [];
+$valueTypes = [];
+
+// Validate and Check What Fields are Available for Update
 
 if(isset($_POST['name']) && is_safeInput($_POST['name'], 'name')) {$table_posted['name'] = $_POST['name'];}
 if(isset($_POST['geolocation']) && is_safeInput($_POST['geolocation'])) {$table_posted['geolocation'] = $_POST['geolocation'];}
 if(isset($_POST['password']) && is_safeInput($_POST['password'], 'password')) {$table_posted['password'] = $_POST['password'];}
 if(isset($_POST['occupation']) && is_safeInput($_POST['occupation'])) {$table_posted['occupation'] = $_POST['occupation'];}
 if(isset($_POST['phone']) && is_safeInput($_POST['phone'], 'phone')) {$table_posted['phone'] = $_POST['phone'];}
-if(isset($_POST['last_logged_ip']) && is_safeInput($_POST['last_logged_ip'])) {$table_posted['last_logged_ip'] = $_POST['last_logged_ip'];}
+if(isset($_POST['status']) && is_safeInput($_POST['status'])) {$table_posted['status'] = $_POST['status'];}
 
-
+// Fields that pass the check are broken into 3 Arrays
 foreach($table_posted as $key => $value) {
-    $sql = mysqli_prepare(con, "UPDATE users SET $key = ? WHERE email = $usr");
-    mysqli_stmt_bind_param($sql, "s", $value);
-    mysqli_stmt_execute($sql);
+    $valueName[] = "$key = ?"; // Contains Field Name
+    $valueData[] = $value; // Contains Field Value
+    $valueTypes[] = "s"; // Contains Field Type
     }
+
+    // PHP Spread Operator ... Used Cause PHP converts Array Values into Comma Seperated Arguments BUT only works inside Functions
+    $commaRemoveTypes = implode("",$valueTypes); 
+    $sql = mysqli_prepare(con, "UPDATE users SET " . implode(",",$valueName) . " WHERE email = $usr");
+    mysqli_stmt_bind_param($sql, $commaRemoveTypes, ...$valueData);
+    mysqli_stmt_execute($sql);
 
 }
 
